357e8ffeecc21be6897c71c755baca85965a4b91
glebkr <77020700+glebkr@users.noreply.github.com>
2021-09-14 00:23:17 +0300

    Shopping List

---
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/data/db/entities/ShoppingDao.kt
@@ -0,0 +1,17 @@
+package com.example.shoppinglist.data.db.entities
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(item: ShoppingItem)
+
+    @Delete
+    fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
+}
